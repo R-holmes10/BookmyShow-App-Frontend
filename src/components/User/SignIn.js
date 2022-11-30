4efbc27ab4394038575066a984a873770aa0929b
Ritu <ritutwinkle10@gmail.com>
2022-11-30 13:52:38 +0530

    minor changes

--- src/components/User/SignIn.js
+++ src/components/User/SignIn.js
@@ -20,6 +20,7 @@ function SignIn() {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
+        'Accept': 'application/json'
       },
       body: JSON.stringify({
         email: email,
